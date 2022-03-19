a6a07131ddfed463166e8b34dadbe20eef5acf77
Phillipe <Phi.coimbra@gmail.com>
2022-03-18 22:49:16 -0300

    First Commit :rocket:

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "itspersonal58df7aaa": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ ios/Pods/Target Support Files/Pods-itspersonal-itspersonalTests/Pods-itspersonal-itspersonalTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_itspersonal_itspersonalTests : NSObject
+@end
+@implementation PodsDummy_Pods_itspersonal_itspersonalTests
+@end
